de87f01006d141fe6f05bdea4fc9ed804a77a893
Roope Kangas <muhmi@iki.fi>
2015-09-11 15:51:50 +0300

    just show name and secret

--- lib/lock.ex
+++ lib/lock.ex
@@ -5,3 +5,10 @@ defmodule Relocker.Lock do
 	defstruct name: "", metadata: nil, secret: "", valid_until: 0, lease_time: 0
 
 end
+
+defimpl Inspect, for: Relocker.Lock do
+  import Inspect.Algebra
+  def inspect(lock, _opts) do
+    concat ["%Relocker.Lock<name: #{inspect lock.name}, secret: #{inspect lock.secret} ...>"]
+  end
+end
