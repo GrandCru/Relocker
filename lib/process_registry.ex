330c4846d49646d1c4bad65f03c34fe15cbf2fcd
Roope Kangas <muhmi@iki.fi>
2015-09-11 15:50:14 +0300

    simplify

--- lib/process_registry.ex
+++ lib/process_registry.ex
@@ -61,7 +61,7 @@ defmodule Relocker.ProcessRegistry do
   end
 
   def send(name, msg) do
-    pid = Relocker.ProcessRegistry.whereis_name(name)
+    pid = whereis_name(name)
     if pid != :undefined do
       Kernel.send(pid, msg)
     else
