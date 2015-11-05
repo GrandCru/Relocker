11896321704a724ed242d55228884bc37ffe5f55
Roope Kangas <muhmi@iki.fi>
2015-11-05 15:24:37 +0200

    Start working on some pooling

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,10 @@
 use Mix.Config
 
 config :relocker,
-  locker: Relocker.Locker.Agent
+  locker: Relocker.Locker.Agent,
+  pool: [
+    name: { :local, Relocker.Locker },
+    worker_module: Relocker.Locker.Pool,
+    size: 10,
+    max_overflow: 0
+  ]
---
+++ lib/locker/pool.ex
@@ -0,0 +1,48 @@
+defmodule Relocker.Locker.Pool do
+
+  alias Relocker.Lock
+  alias Relocker.Utils
+
+  @behaviour Relocker.Locker
+
+  def child_spec do
+    :poolboy.child_spec __MODULE__, Application.fetch_env!(:relocker, :pool)
+  end
+
+  def start_link(_args) do
+    GenServer.start_link Relocker.Locker.Redis
+  end
+
+  def lock(name, metadata, lease_time_secs, time) do
+    :poolboy.transaction __MODULE__, fn pid -> 
+      GenServer.call pid, {:lock, name, metadata, lease_time_secs, time}
+    end
+  end
+
+  def read(name, time) do
+    :poolboy.transaction __MODULE__, fn pid -> 
+      GenServer.call pid, {:read, name, time}
+    end
+  end
+
+  def extend(%Lock{} = lock, time) do
+    :poolboy.transaction __MODULE__, fn pid -> 
+      GenServer.call pid, {:extend, lock, time}
+    end
+  end
+
+  def unlock(%Lock{} = lock, time) do
+    :poolboy.transaction __MODULE__, fn pid -> 
+      GenServer.call pid, {:unlock, lock, time}
+    end
+  end
+
+  def reset do
+    :ok
+  end
+
+  def stop do
+    :ok
+  end
+
+end
