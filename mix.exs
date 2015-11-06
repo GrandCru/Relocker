253f7799b6c9bd3f0d22641665eb99b77bcdc9f3
Roope Kangas <muhmi@iki.fi>
2015-11-06 11:19:15 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Relocker.Mixfile do
 
   def project do
     [app: :relocker,
-     version: "0.0.5",
+     version: "0.0.6",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
