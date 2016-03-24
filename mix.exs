ca9d665e91a4e6b2b113e2882e2c2aba77337c9e
Roope Kangas <muhmi@iki.fi>
2016-03-24 10:03:38 +0200

    Switch to a new version of exredis

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Relocker.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:exredis, ">= 0.2.0" },
+      {:exredis, ">= 0.2.3" },
       {:poolboy, github: "devinus/poolboy", tag: "1.5.1"},
       {:ex_doc, "~> 0.9", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev}
